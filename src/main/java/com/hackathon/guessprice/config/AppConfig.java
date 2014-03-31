7037181396eba83576b84de88d19b1769dfbbd6a
Capricz <adermon_1224@126.com>
2014-03-31 12:56:46 +0800

    make the app run

--- src/main/java/com/hackathon/guessprice/config/AppConfig.java
+++ src/main/java/com/hackathon/guessprice/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.hackathon.guessprice.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.hackathon.guessprice")
 public class AppConfig {
 
 }
--- src/main/java/com/hackathon/guessprice/config/WebConfig.java
+++ src/main/java/com/hackathon/guessprice/config/WebConfig.java
@@ -1,10 +1,20 @@
 package com.hackathon.guessprice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan("com.hackathon.guessprice")
 public class WebConfig {
-
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
 }
