33e364197dcbcc4589ca78b760ee2b08b4267c58
Capricz <adermon_1224@126.com>
2014-04-03 10:46:40 +0800

    add UserPercentItem

---
+++ src/main/java/com/hackathon/guessprice/model/UserPercentItem.java
@@ -0,0 +1,22 @@
+package com.hackathon.guessprice.model;
+
+public class UserPercentItem {
+	private String region;
+	private Double percent;
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public Double getPercent() {
+		return percent;
+	}
+
+	public void setPercent(Double percent) {
+		this.percent = percent;
+	}
+}
