81ce9b06944aad86fddf4c93c68405e81cb9393c
Capricz <adermon_1224@126.com>
2014-04-08 08:22:58 +0800

    1.add Snippet sample 2.summarize function list

---
+++ src/test/java/snippet/Snippet.java
@@ -0,0 +1,17 @@
+/*package com.baobaotao.web;
+import org.springframework.ui.ModelMap;  
+import org.springframework.web.bind.annotation.SessionAttributes;    
+
+@Controller  
+@RequestMapping("/bbtForum.do")  
+@SessionAttributes("currUser") //①将ModelMap中属性名为currUser的属性  //放到Session属性列表中，以便这个属性可以跨请求访问  
+public class BbtForumController {
+	@RequestMapping(params = "method=listBoardTopic")
+	public String listBoardTopic(@RequestParam("id")int topicId, User user,  ModelMap model) {
+		bbtForumService.getBoardTopics(topicId);
+		System.out.println("topicId:" + topicId);
+		System.out.println("user:" + user);
+		model.addAttribute("currUser",user); //②向ModelMap中添加一个属性          
+		return "listTopic";     
+	}
+}*/
